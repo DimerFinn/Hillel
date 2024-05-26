4d23b0db8ab43fcef38027fb4b25794f9f81b25f
DimerFinn <dmitriy.finochek@gmail.com>
2024-05-26 15:27:48 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,19 @@
 function DataType() {
     let MyName = 'Dmytro';
     let MyAge = 45;
+    let MyLifeTime = 14200704005245879665412364412665478963214;
     let MyCountry = 'Ukraine';
     let MyGenderMan = true;
+    let MyNumberID = 123654;
     let MycriminalRecord = null;
+    let Mychildrens = ['Vlada','Ivanna', 'Lana', 'Mykola'];
     console.log(MyName, ' - type of variable:', typeof MyName);
     console.log(MyAge,' - type of variable:', typeof MyAge);
+    console.log(MyLifeTime,' - type of variable:', typeof MyLifeTime);
     console.log(MyCountry,' - type of variable:', typeof MyCountry);
     console.log(MyGenderMan,' - type of variable:', typeof MyGenderMan);
+    console.log(MyNumberID,' - type of variable:', typeof MyNumberID);
     console.log(MycriminalRecord,' - type of variable:', typeof MycriminalRecord);
+    console.log(Mychildrens,' - type of variable:', typeof Mychildrens);
 };  
 DataType();
