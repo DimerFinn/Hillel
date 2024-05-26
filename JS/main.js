217284f56121c6dc8d8e56b15b353fec58871430
DimerFinn <dmitriy.finochek@gmail.com>
2024-05-26 14:40:37 +0200

    first

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,13 @@
-console.log('Hello world!!!');
+function DataType() {
+    let MyName = 'Dmytro';
+    let MyAge = 45;
+    let MyCountry = 'Ukraine';
+    let MyGenderMan = true;
+    let MycriminalRecord = null;
+    console.log(MyName, ' - type of variable:', typeof MyName);
+    console.log(MyAge,' - type of variable:', typeof MyAge);
+    console.log(MyCountry,' - type of variable:', typeof MyCountry);
+    console.log(MyGenderMan,' - type of variable:', typeof MyGenderMan);
+    console.log(MycriminalRecord,' - type of variable:', typeof MycriminalRecord);
+};  
+DataType();
