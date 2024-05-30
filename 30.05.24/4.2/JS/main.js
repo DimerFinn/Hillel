afc2b960399cd6fa0ca46f7c7cc19c0f178c5c2a
DimerFinn <dmitriy.finochek@gmail.com>
2024-05-30 17:06:48 +0200

    4.2

--- 30.05.24/4.2/JS/main.js
+++ 30.05.24/4.2/JS/main.js
@@ -1,8 +1,14 @@
 let arr = (prompt("Enetr your numbers"));
 let arrNumbers = arr;
-if (arrNumbers[0] === arrNumbers[1] && arrNumbers[1] === arrNumbers[2]) {
+    if (arrNumbers[0] === arrNumbers[1] && arrNumbers[1] === arrNumbers[2]) {
 console.log("Цифри однакові");
 } 
-else {
+    else {
 console.log("Цифри не одинакові");
 }   
+    if (arrNumbers[0] === arrNumbers[1] || arrNumbers[1] === arrNumbers[2]) {
+console.log("Є однакові цифри ");
+} 
+    else {
+        console.log("Немає однакових цифр");
+}
