efbd20d7154860bd91f3a887c190de62fb9676c7
DimerFinn <dmitriy.finochek@gmail.com>
2024-06-09 14:41:39 +0200

    5.3

---
+++ 5/5.3/main.js
@@ -0,0 +1,11 @@
+    let N = prompt('input number');
+    for (i=1; i<=100; i++) {
+        let x = Math.pow(i,2);
+        if (x<=N) {
+        
+            
+        console.log(i);
+        }
+    }
+
+
