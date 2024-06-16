0d475cc0b6ebae9117f28969b981f8a21f765cdc
DimerFinn <dmitriy.finochek@gmail.com>
2024-06-16 20:32:10 +0200

    7.3

---
+++ 7/7.3/main.js
@@ -0,0 +1,18 @@
+for (let i=0; i<10; i++) {
+    let MyNum = prompt ('Введіть число більше 100');
+
+    if (MyNum<100) {
+        alert ('Спробуйте ще раз!');
+        console.log(MyNum);
+    } 
+    else if (MyNum=>100 || MyNum (typeof 'string')){ 
+        alert ('Функція завершена');
+        console.log(MyNum);
+        break
+    } 
+}
+
+
+
+
+
