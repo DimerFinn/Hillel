8bacad04f0a175b621dc9dfbf7858266338a3288
DimerFinn <dmitriy.finochek@gmail.com>
2024-06-23 15:20:46 +0200

    11.2

--- 11/11.2/main.js
+++ 11/11.2/main.js
@@ -1,22 +1,12 @@
-// Отримуємо необхідні елементи з DOM
 const textBlock = document.getElementById('text-block');
 const changeColorBtn = document.getElementById('change-color-btn');
-
-// Масив з доступними кольорами
 const colors = ['red', 'green', 'blue', 'yellow', 'purple'];
 
-let currentIndex = 0; // Індекс поточного кольору
-
-// Додаємо обробник події для кнопки
+let currentIndex = 0;
 changeColorBtn.addEventListener('click', function() {
-  // Змінюємо колір тексту
-  textBlock.style.color = colors[currentIndex];
-  
-  // Збільшуємо індекс для наступного кольору
-  currentIndex++;
-  
-  // Якщо досягли кінця масиву кольорів, повертаємося до початку
-  if (currentIndex >= colors.length) {
+textBlock.style.color = colors[currentIndex];
+currentIndex++;
+if (currentIndex >= colors.length) {
     currentIndex = 0;
   }
 });
